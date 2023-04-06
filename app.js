eec456d085ba2d2feb8511f48df9b72231a35306
Normi029-RTHERY <rom.t95@free.fr>
2023-04-06 12:59:20 +0200

    s

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 
 const plane = document.getElementById('msg-ico')
 function fly() {
-    const newPlane = plane.cloneNode(true);
-    var coords = getAbsPosition(plane)
-    document.body.appendChild(newPlane)
+    // const newPlane = plane.cloneNode(true);
+    // var coords = getAbsPosition(plane)
+    // document.body.appendChild(newPlane)
+    document.getElementById("msg-ico").className="msg-ico-active"
 }
 
 function getAbsPosition(element) {
